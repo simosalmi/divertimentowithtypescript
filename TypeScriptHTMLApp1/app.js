af40b434b208fd5cc1405740e319569b692f0009
Simone Salmistraro <simo.salmi@gmail.com>
2015-05-02 20:48:31 +0100

    First commit

---
+++ TypeScriptHTMLApp1/app.js
@@ -0,0 +1,36 @@
+﻿/*
+* Inspired by "Encapsulation and SOLID" Pluralsight Course
+* by Mark Seemann
+*/
+var Maybe = (function () {
+    function Maybe(element) {
+        var _zeroOrOneElement;
+        if (element !== null && element !== undefined) {
+            _zeroOrOneElement = [element];
+        } else {
+            _zeroOrOneElement = [];
+        }
+
+        this.isEmpty = function () {
+            return _zeroOrOneElement.length === 0;
+        };
+
+        this.firstOrDefaultIfEmpty = function (defaultValue) {
+            if (this.isEmpty()) {
+                return defaultValue;
+            } else {
+                return _zeroOrOneElement[0];
+            }
+        };
+    }
+    return Maybe;
+})();
+
+var testMaybe = new Maybe();
+console.log(testMaybe.firstOrDefaultIfEmpty('default'));
+
+// prints 'default'
+var testMaybe2 = new Maybe('a real string');
+console.log(testMaybe2.firstOrDefaultIfEmpty('default'));
+// prints 'a real string'
+//# sourceMappingURL=app.js.map
---
+++ TypeScriptHTMLApp1/app.ts
@@ -0,0 +1,39 @@
+﻿/*
+ * Inspired by "Encapsulation and SOLID" Pluralsight Course 
+ * by Mark Seemann
+ */
+class Maybe<T> {
+    isEmpty : () => boolean;
+    firstOrDefaultIfEmpty : (T) => T;
+
+    constructor(element?: T) {
+        var _zeroOrOneElement: Array<T>;
+        if (element !== null && element !== undefined) {
+            _zeroOrOneElement = [element];
+        }
+        else {
+            _zeroOrOneElement = [];
+        }
+
+        this.isEmpty = function() : boolean {
+            return _zeroOrOneElement.length === 0;
+        }
+
+        this.firstOrDefaultIfEmpty = function(defaultValue : T) : T {
+            if (this.isEmpty()) {
+                return defaultValue;
+            }
+            else {
+                return _zeroOrOneElement[0];
+            }
+        }
+    }
+}
+
+var testMaybe = new Maybe<string>();
+console.log(testMaybe.firstOrDefaultIfEmpty('default'));
+// prints 'default'
+
+var testMaybe2 = new Maybe<string>('a real string');
+console.log(testMaybe2.firstOrDefaultIfEmpty('default'));
+// prints 'a real string'
